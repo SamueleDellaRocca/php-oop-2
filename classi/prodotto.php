a28cd108ae45896111b1534d9905ccc66ff3dceb
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-05-02 15:20:25 +0200

    p

---
+++ classi/prodotto.php
@@ -0,0 +1,31 @@
+<?php
+
+class Prodotto
+{
+    protected $marca;
+    protected $prezzo;
+
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+
+        return $this;
+    }
+
+    public function getPrezzo()
+    {
+        return $this->prezzo;
+    }
+
+    public function setPrezzo($prezzo)
+    {
+        $this->prezzo = $prezzo;
+
+        return $this;
+    }
+};
